b7b669863cdc4788714709f47fa54972799bbe0c
Nguyen Hoang Quan <107245089+BlackIcon058@users.noreply.github.com>
2024-04-04 12:32:35 +0700

    Update login_user.php

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['EMAIL_USER_LOGIN'] = $row['email'];
             $_SESSION['USERNAME_USER_LOGIN'] = $row['name'];
             $_SESSION['user_id'] = $row['id'];
-            $_SESSION['message'] = 'Đăng nhập thành công!';
+            //$_SESSION['message'] = 'Đăng nhập thành công!';
         }
     } else {
         echo 'Invalid';
