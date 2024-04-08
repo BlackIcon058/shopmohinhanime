7bf24b6e4453cd481ec6435407c9550e9fdeab75
ngocchau04 <damthingocchau26@gmail.com>
2024-04-08 10:51:22 +0700

    up

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $current_page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 <div class="banner-container">
   <div class="banner">
     <div class="banner-text">
-      <h1 style="color: red ; text-shadow: 2px 2px #00ff40;"><i>WELCOME TO hahaha ANIME HEAVEN</i></h1>
+      <h1 style="color: red ; text-shadow: 2px 2px #00ff40;"><i>WELCOME TO ANIME HEAVEN</i></h1>
       <a href="#product" class="banner-btn">SEE MORE</a>
     </div>
   </div>
